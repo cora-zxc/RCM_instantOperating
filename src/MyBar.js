15756157649832b921047b109fbdf6ae277348e4
cora-zxc <free0610on@gmail.com>
2021-06-09 08:51:06 +0800

    20210609_01

--- src/MyBar.js
+++ src/MyBar.js
@@ -4,148 +4,148 @@ import { Column } from '@ant-design/charts';
 import { Area } from "@ant-design/charts";
 
 const DemoColumn: React.FC = () => {
-    var data = [
-      {
-        date: '05-30',
-        value: 82,
-      },
-      {
-        date: '05-31',
-        value: 74,
-      },
-      {
-        date: '06-01',
-        value: 70,
-      },
-      {
-        date: '06-02',
-        value: 78,
-      },
-      {
-        date: '06-03',
-        value: 88,
-      },
-      {
-        date: '06-04',
-        value: 90,
-      },
-      {
-        date: '06-05',
-        value: 83,
-      },
-      {
-        date: '06-06',
-        value: 70,
-      },
-      {
-        date: '06-07',
-        value: 89,
-      },
-      {
-        date: '06-08',
-        value: 96,
-      }
-    ];
-    var config = {
-      data: data,
-      xField: 'date',
-      yField: 'value',
-      label: {
-        position: 'middle',
-        style: {
-          fill: '#ffffff',
-          opacity: 0.7
-        },
-      },
-      meta: {
-        date: { alias: 'date' },
-        value: { min: 0, max: 100 },
-      },
-      tooltip: true,
-      columnWidthRatio: 0.5,
-    };
-    return <Column {...config} />;
-};
-const areainfo = [
+  var data = [
     {
-        date: '05-30',
-        value: 82,
+      date: '05-30',
+      value: 82,
     },
     {
-        date: '05-31',
-        value: 74,
+      date: '05-31',
+      value: 74,
     },
     {
-        date: '06-01',
-        value: 70,
+      date: '06-01',
+      value: 70,
     },
     {
-        date: '06-02',
-        value: 78,
+      date: '06-02',
+      value: 78,
     },
     {
-        date: '06-03',
-        value: 88,
+      date: '06-03',
+      value: 88,
     },
     {
-        date: '06-04',
-        value: 90,
+      date: '06-04',
+      value: 90,
     },
     {
-        date: '06-05',
-        value: 83,
+      date: '06-05',
+      value: 83,
     },
     {
-        date: '06-06',
-        value: 70,
+      date: '06-06',
+      value: 70,
     },
     {
-        date: '06-07',
-        value: 89,
+      date: '06-07',
+      value: 89,
     },
     {
-        date: '06-08',
-        value: 96,
+      date: '06-08',
+      value: 96,
     }
-];
+  ];
+  var config = {
+    data: data,
+    xField: 'date',
+    yField: 'value',
+    label: {
+      position: 'middle',
+      style: {
+        fill: '#ffffff',
+        opacity: 0.7
+      },
+    },
+    meta: {
+      date: { alias: 'date' },
+      value: { min: 0, max: 100 },
+    },
+    tooltip: true,
+    columnWidthRatio: 0.5,
+  };
+  return <Column {...config} />;
+};
 const DemoArea: React.FC = () => {
-    var config = {
-        data: areainfo,
-        xField: "date",
-        yField: "value",
-        areaStyle: { fillOpacity: 0.7 },
-        appendPadding: 10,
-        isPercent: false,
-        tooltip: true,
-        yAxis: {
-            label: {
-            formatter: function formatter(value) {
-                return value;
-            }
-            }
+  var areainfo = [
+    {
+      date: '05-30',
+      value: 82,
+    },
+    {
+      date: '05-31',
+      value: 74,
+    },
+    {
+      date: '06-01',
+      value: 70,
+    },
+    {
+      date: '06-02',
+      value: 78,
+    },
+    {
+      date: '06-03',
+      value: 88,
+    },
+    {
+      date: '06-04',
+      value: 90,
+    },
+    {
+      date: '06-05',
+      value: 83,
+    },
+    {
+      date: '06-06',
+      value: 70,
+    },
+    {
+      date: '06-07',
+      value: 89,
+    },
+    {
+      date: '06-08',
+      value: 96,
+    }
+  ]
+  var config = {
+    data: areainfo,
+    xField: "date",
+    yField: "value",
+    areaStyle: { fillOpacity: 0.7 },
+    appendPadding: 10,
+    isPercent: false,
+    tooltip: true,
+    yAxis: {
+      label: {
+        formatter: function formatter(value) {
+            return value;
         }
-    };
-    return <Area {...config} />;
+      }
+    }
+  };
+  return <Area {...config} />;
 };
   
 class MyBar extends React.Component {
-    constructor(props){
-        super(props);
-        this.title=["近期設備稼動率分析","近期設備用時分析"];
-    }
-    render() {
-        return (
-        <div id="bar">
-            <div>
-                <div>{this.title[0]}</div>
-                <div><DemoColumn /></div>
-            </div>
-            <div>
-                <div>{this.title[1]}</div>
-                <div><DemoArea /></div>
-            </div>
-        </div>
-        );
-    }
+  constructor(props){
+    super(props);
+    this.title=["近期設備稼動率分析","近期設備用時分析"];
+  }
+  render() {
+    return (
+    <div id="bar">
+      <div>
+        <div>{this.title[0]}</div>
+        <div><DemoColumn /></div>
+      </div>
+      <div>
+        <div>{this.title[1]}</div>
+        <div><DemoArea /></div>
+      </div>
+    </div>
+    );
+  }
 }
 export default MyBar
--- src/MyPie.js
+++ src/MyPie.js
@@ -4,133 +4,135 @@ import { Gauge } from '@ant-design/charts';
 import { Pie } from '@ant-design/charts';
 
 const DemoGauge1: React.FC = () => {
-    var config = {
-        innerRadius: 0.5,
-        percent: 0.75,
-        range: { color: '#3542e6' },
-        startAngle: Math.PI,
-        endAngle: 2 * Math.PI,
-        indicator: null,
-        statistic: {
-            title: {
-                offsetY: 0,
-                style: {
-                    fontSize: '36px',
-                    color: '#4B535E',
-                },
-                formatter: function formatter() {
-                    return '70%';
-                },
-            },
+  var config = {
+    innerRadius: 0.5,
+    percent: 0.75,
+    range: { color: '#3542e6' },
+    startAngle: Math.PI,
+    endAngle: 2 * Math.PI,
+    indicator: null,
+    statistic: {
+      title: {
+        offsetY: 0,
+        style: {
+          fontSize: '36px',
+          color: '#4B535E',
         },
-    };
-    return <Gauge {...config} />;
+        formatter: function formatter() {
+          return '70%';
+        },
+      },
+    },
+  };
+  return <Gauge {...config} />;
 };
 const DemoGauge2: React.FC = () => {
-    var config = {
-        innerRadius: 0.5,
-        percent: 0.75,
-        range: { color: '#3542e6' },
-        startAngle: Math.PI,
-        endAngle: 2 * Math.PI,
-        indicator: null,
-        statistic: {
-            title: {
-                offsetY: -10,
-                style: {
-                    fontSize: '36px',
-                    color: '#4B535E',
-                },
-                formatter: function formatter() {
-                    return '70%';
-                },
-            },
+  var config = {
+    innerRadius: 0.5,
+    percent: 0.75,
+    range: { color: '#3542e6' },
+    startAngle: Math.PI,
+    endAngle: 2 * Math.PI,
+    indicator: null,
+    statistic: {
+      title: {
+        offsetY: -10,
+        style: {
+          fontSize: '36px',
+          color: '#4B535E',
         },
-    };
-    return <Gauge {...config} />;
+        formatter: function formatter() {
+          return '70%';
+        },
+      },
+    },
+  };
+  return <Gauge {...config} />;
 };  
 const DemoPie: React.FC = () => {
-    var data = [
-        {
-          type: '運行',
-          value: 80,
+  var data = [
+    {
+      type: '運行',
+      value: 80,
+    },
+    {
+      type: '關機',
+      value: 6,
+    },
+    {
+      type: '報警',
+      value: 2,
+    },
+    {
+      type: '停機',
+      value: 6,
+    },
+    {
+      type: '待機',
+      value: 12,
+    }
+  ];
+  var config = {
+    innerRadius: 0.6,
+    statistic: {
+      title: false,
+      content: {
+        formatter: function formatter() {
         },
-        {
-          type: '關機',
-          value: 6,
-        },
-        {
-          type: '報警',
-          value: 2,
-        },
-        {
-          type: '停機',
-          value: 6,
-        },
-        {
-          type: '待機',
-          value: 12,
-        }
-      ];
-      var config = {
-        //新加的
-        innerRadius: 0.6,
-        statistic: {
-            title: false,
-            content: {
-              formatter: function formatter() {
-              },
-            },
-          },
-        tooltip: false, 
-        legend: false, 
-        //
-        appendPadding: 10,
-        data: data,
-        angleField: 'value',
-        colorField: 'type',
-        color: ({ type }) => {
-          if(type === '運行'){
-            return '#41e16c';
-          }
-          if(type === '關機'){
-            return '#f62526';
-          }
-          if(type === '報警'){
-            return '#f1d326';
-          }
-          if(type === '停機'){
-            return '#9a9a9a';
-          }
-          if(type === '待機'){
-            return '#092278';
-          };
-        },
-        radius: 0.9,
-        label: {
-          type: 'spider',
-          labelHeight: 50,
-          content: '{name}\n{percentage}',
       },
-      interactions: [{ type: 'element-active' }],
-    };
-    return <Pie {...config} />;
+    },
+    tooltip: true, 
+    legend: false, 
+    appendPadding: 10,
+    data: data,
+    angleField: 'value',
+    colorField: 'type',
+    color: ({ type }) => {
+      if(type === '運行'){
+        return '#41e16c';
+      }
+      if(type === '關機'){
+        return '#f62526';
+      }
+      if(type === '報警'){
+        return '#f1d326';
+      }
+      if(type === '停機'){
+        return '#9a9a9a';
+      }
+      if(type === '待機'){
+        return '#092278';
+      };
+    },
+    radius: 0.9,
+    label: {
+      type: 'spider',
+      labelHeight: 50,
+      content: '{name}\n{percentage}',
+    },
+    interactions: [{ type: 'element-active' }],
   };
+  return <Pie {...config} />;
+};
 
 class MyPie extends React.Component {
+  constructor(props){
+    super(props);
+    this.title=["設備稼動率","當前機台狀態分析","設備關機比率"];
+  }
     render() {
         return (
         <div id="pie">
             <div>
-                <div>設備稼動率</div>
+                <div>{this.title[0]}</div>
                 <div><DemoGauge1 /></div>
             </div>
             <div>
-                <div>當前機台狀態分析</div>
+                <div>{this.title[1]}</div>
                 <div><DemoPie /></div>
             </div>
             <div>
-                <div>設備關機比率</div>
+                <div>{this.title[2]}</div>
                 <div><DemoGauge2 /></div>
             </div>
         </div>
